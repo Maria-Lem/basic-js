6ae9a2ae7e1ce1f97e9c558b53d2a197431e1a55
Maria <maria131621@yandex.ru>
2020-10-22 22:59:49 +0300

    feat: add countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  const catsInArray = matrix.includes('^^');
-  const count = {};
-  if (catsInArray === true) return catsInArray.forEach(function(x) {count[x] = (count[x] || 0) + 1;});
+  let catsInArray = matrix.flat().includes('^^');
+  const val = '^^';
+  if (catsInArray === true) {
+    return matrix.flat().reduce((a, v) => (v === val ? a + 1 : a), 0);
+  } else {
+    return 0;
+  }
 };
