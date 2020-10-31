26fff853bf26fe23969dcf79cff9b774cfc60d30
Maria <maria131621@yandex.ru>
2020-10-31 23:25:37 +0300

    feat: add recursive depth solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  // if 
+  if (Array.isArray(arr)) {
+    if (arr.includes('--discard-next')){
+      // const discardNext = arr.indexOf('--discard-next');
+      arr.splice(arr.indexOf('--discard-next'), 2);
+      return arr;
+    } else if (arr.includes('--double-prev')) {
+      const doublePrev = arr.indexOf('--double-prev');
+      arr.splice(doublePrev, -1);
+      return arr;
+    }
+  } else if (Array.isArray(arr) && arr.length === 0) {
+    let empty = [];
+    return empty;
+  } else {
+    throw new Error();
+  }
 };
